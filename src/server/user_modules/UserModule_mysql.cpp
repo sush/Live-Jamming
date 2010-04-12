07aebeae688d11c044b50d6301153bf930b52791
petit_y <petit_y@833c2cf1-1b31-df11-b905-005056bd6404>
2010-04-12 15:16:48 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@38 833c2cf1-1b31-df11-b905-005056bd6404

--- src/server/user_modules/UserModule_mysql.cpp
+++ src/server/user_modules/UserModule_mysql.cpp
@@ -6,13 +6,13 @@ UserModule_mysql::~UserModule_mysql() {}
 
 int UserModule_mysql::Connection() {
   // define connection informations in an yaml file
-  const char* db = "live-jamming_db", *server = 0, *user = "root", *pass = "MAgoun12$";
-  mysqlpp::Connection _connection = _connection.connect(db, server, user, pass);
+  const char* db = "live-jamming_db", *server = "192.168.0.12", *user = "root", *pass = "MAgoun12$";
+  mysqlpp::Connection _connection(false);
   
-  if(!_connection)
-    return 1;
-  else
+  if(_connection.connect(db, server, user, pass))
     return 0;
+  else
+    return 1;
 }
 
 int UserModule_mysql::Authentification() {
