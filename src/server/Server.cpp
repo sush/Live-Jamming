7f1ea0d7152adaaf607199e3626bc81d5fbe6f59
o-conn_g <o-conn_g@833c2cf1-1b31-df11-b905-005056bd6404>
2010-04-15 19:25:18 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@43 833c2cf1-1b31-df11-b905-005056bd6404

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -2,19 +2,58 @@
 
 namespace lj
 {
+
+  const std::string	Server::_address = "192.168.0.13";
+  const int		Server::_port	= 5042;
+  const int		Server::_poolSize = 8;
+
   void		Server::Run()
   {
     std::cout << "Server started..." << std::endl;
+ 
+    _pool = new boost::threadpool::pool(_poolSize);
+    start_receive();
+    _io_service.run();
   }
 
+
+  void Server::start_receive()
+  {
+    _recv_buffer = new Packet::buffer_t;
+    _socket->async_receive_from(boost::asio::buffer(*_recv_buffer), _remote_endpoint,
+				boost::bind(&Server::CallBack_handle_receive, this,
+					    boost::asio::placeholders::error,
+					    boost::asio::placeholders::bytes_transferred));
+  }
+
+  void	Server::CallBack_handle_receive(boost::system::error_code const & error, std::size_t recv_count)
+  {
+    std::cout << "receive" << std::endl;
+    if (!error || error == boost::asio::error::message_size)
+      {
+	_packetQueue->PushPacket(new Packet(_recv_buffer, recv_count));
+	_pool->schedule(boost::bind(&Server::Thread_task, this));
+	start_receive();
+      }
+  }
+
+  void		Server::Thread_task()
+  {
+    Packet	*packet = _packetQueue->PopPacket();
+    packet->Print();
+    delete packet;
+  }
 
   void		Server::Init(int argc, char *argv[])
   {
     _argc = argc;
     _argv = argv;
-
+    _local_endpoint = new boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string(_address.c_str()), _port);
+ 
+    _socket = new boost::asio::ip::udp::socket (_io_service);
+    _socket->open(boost::asio::ip::udp::v4());
+    _socket->bind(*_local_endpoint);
+    _packetQueue = new PacketQueue;
   }
-
-
 
 }
--- src/server/include/Server.h
+++ src/server/include/Server.h
@@ -1,7 +1,16 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
 
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <boost/threadpool.hpp>
+
 #include <iostream>
+#include <Packet.h>
+#include <PacketQueue.h>
 #include <Singleton.h>
 
 namespace lj
@@ -12,13 +21,27 @@ namespace lj
     friend class Singleton<Server>;
 
   public:
-    void	Run();
-    void	Init(int, char *[]);
+    void			Run();
+    void			Init(int, char *[]);
     
   private:
+    void			Thread_task();
+    void			start_receive();
+    void			CallBack_handle_receive(boost::system::error_code const &, std::size_t);
 
-    int		_argc;
-    char 	**_argv;
+    int				_argc;
+    char			**_argv;
+    static const std::string	_address;
+    static const int		_port;
+    static const int		_poolSize;
+
+    boost::threadpool::pool		*_pool;
+    boost::asio::ip::udp::socket	*_socket;
+    boost::asio::ip::udp::endpoint	*_local_endpoint;
+    boost::asio::io_service		_io_service;
+    boost::asio::ip::udp::endpoint	_remote_endpoint;
+    Packet::buffer_t			*_recv_buffer;
+    PacketQueue				*_packetQueue;
   };
 }
 
