9d03b67bc8ab06ae5a5f946f5c7e17851d706a77
o-conn_g <o-conn_g@833c2cf1-1b31-df11-b905-005056bd6404>
2010-04-19 15:46:45 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@79 833c2cf1-1b31-df11-b905-005056bd6404

--- src/server/logger_modules/include/Syslog.h
+++ src/server/logger_modules/include/Syslog.h
@@ -1,3 +1,5 @@
+#ifndef __SYSLOG_H__
+#define __SYSLOG_H__
 #include <string>
 #include <syslog.h>
 #include <ILoggerModule.h>
@@ -11,3 +13,6 @@ class Syslog : public ILoggerModule<Syslog> {
   virtual ~Syslog();
   void AddLine(int, std::string const &);
 };
+
+#endif // ! __SYSLOG_H__
+
