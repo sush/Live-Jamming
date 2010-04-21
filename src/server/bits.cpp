b47ce77657288832364844e4a436342a3b2d0074
petit_y <petit_y@833c2cf1-1b31-df11-b905-005056bd6404>
2010-04-21 15:17:30 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@94 833c2cf1-1b31-df11-b905-005056bd6404

--- src/server/bits.cpp
+++ src/server/bits.cpp
@@ -7,7 +7,7 @@
 //            5    9
 // decimal = 1493172224
 
-// [4bits][3bit][1bit][9bit][8bit][7bit]          <=> raw
+// [4bits][3bit][1.bit][9bit][8bit][7bit]          <=> raw
 // c1	   c2    c3    c4    c5    c6     32bit   <=> raw
 
 int	c1_size = 4;
@@ -17,33 +17,57 @@ int	c4_size = 9;
 int	c5_size = 8;
 int	c6_size = 7;
 
+
+struct bits {
+  unsigned int f1 : 4;
+  unsigned int f2 : 3;
+  unsigned int f3 : 1;
+  unsigned int f4 : 9;
+  unsigned int f5 : 8;
+  unsigned int f6 : 7;
+} field;
+
+
 int	MakePacket(int c1, int c2, int c3, int c4, int c5, int c6)
 {
   int	raw;
+  field.f1 = 15;
 
+  //  std::cout << field << std::endl;
+
+//   unsigned int test = c1 << sizeof_bin(c1) - c1_size;
+//   std::cout << test << std::endl;
   //make raw
   return raw;
 }
 
 int	ReadPacket(int raw)
 {
-  int	c1, c2, c3, c4, c5, c6;
+  unsigned int	c1, c2, c3, c4, c5, c6;
+
+  std::cout << sizeof_bin(raw) << std::endl;
+  c1 = raw >> (sizeof_bin(raw) - c1_size);
+  c2 = raw >> (sizeof_bin(raw) - c2_size);
+  std::cout << "C1 = " << c1 << std::endl;
+  std::cout << "C2 = " << c2 << std::endl;
 
   // get and print values from raw
 }
 
-void	print()
-{
-  unsigned int	first_octet = raw >> (sizeof_bin(raw)  - 8);
-  unsigned int	sessionId   = raw >> (sizeof_bin(raw) * 8 - sessionID_size);
-  unsigned int	champs2   = raw << sessionID_size >> (sizeof_bin(raw) - champs2_size);
-  std::cout << "first_octet = " << first_octet << std::endl;
-  std::cout << "sessionID = " << sessionId << std::endl;
-  std::cout << "champs2 = " << champs2 << std::endl;
-}
+// void	print()
+// {
+//   unsigned int	first_octet = raw >> (sizeof_bin(raw)  - 8);
+//   unsigned int	sessionId   = raw >> (sizeof_bin(raw) * 8 - sessionID_size);
+//   unsigned int	champs2   = raw << sessionID_size >> (sizeof_bin(raw) - champs2_size);
+//   std::cout << "first_octet = " << first_octet << std::endl;
+//   std::cout << "sessionID = " << sessionId << std::endl;
+//   std::cout << "champs2 = " << champs2 << std::endl;
+// }
 
 int	main()
 {
-  print();
+  //  int raw = MakePacket(1, 2, 3, 4, 5, 6);
+  //  ReadPacket(raw);
+  ReadPacket(1234567891);
   return 0;
 }
--- src/server/main2.cpp
+++ src/server/main2.cpp
@@ -2,9 +2,9 @@
 // #include <UserModule_mysql.h>
 
 //#include "Config.h"
-#include "Argument.h"
-#include "Syslog.h"
-
+//#include "Argument.h"
+//#include "Syslog.h"
+#include "Forge.h"
 int main(int argc, char *argv[]) {
   // authentification test
   //   UserModule_mysql *userModule = UserModule_mysql::getInstance();
@@ -13,8 +13,10 @@ int main(int argc, char *argv[]) {
 
   // Config & Arguments test
   //Config *conf = new Config("config.yaml", argc, argv);
-  Syslog *syslog = new Syslog();
-  syslog->AddLine(1, "TEST LIVE-JAMMING!!!!!!!!!!!!");
+  // Syslog *syslog = new Syslog();
+  //   syslog->AddLine(1, "TEST LIVE-JAMMING!!!!!!!!!!!!");
   //  Argument *args = new Argument(argc, argv);
+  Forge *forge = new Forge();
+  forge->ForgePacket();
   return 0;
 }
