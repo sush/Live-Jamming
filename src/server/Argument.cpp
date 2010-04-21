cdd3dd412782573afcef4dcb707128257b9a9b2c
petit_y <petit_y@833c2cf1-1b31-df11-b905-005056bd6404>
2010-04-21 09:24:18 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@89 833c2cf1-1b31-df11-b905-005056bd6404

--- src/server/Argument.cpp
+++ src/server/Argument.cpp
@@ -8,17 +8,24 @@ Argument::~Argument() {}
 
 int Argument::BuildCommandLine(int argc, char** const & argv) {
   boost::program_options::options_description desc("Usage: livejammin_server [options]\nAllowed options");
-  desc.add_options()
-    ("help,h", "produce help message")
-    ("HostName,H", boost::program_options::value<std::vector<std::string> >(), "set server hostname")
-    ("Port,p", boost::program_options::value<std::vector<std::string> >(), "set server port")
-    ("BindAdress,b", boost::program_options::value<std::vector<std::string> >(), "set server bind(s) address(es)");
-  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), _args);
-  boost::program_options::notify(_args);
-  
-  if (_args.count("help")) {
-    std::cout << desc << "\n";
-    exit(0);
+
+  try {
+    desc.add_options()
+      ("help,h", "produce help message")
+      ("HostName,H", boost::program_options::value<std::vector<std::string> >(), "set server hostname")
+      ("Port,p", boost::program_options::value<std::vector<std::string> >(), "set server port")
+      ("BindAdress,b", boost::program_options::value<std::vector<std::string> >(), "set server bind(s) address(es)");
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), _args);
+    boost::program_options::notify(_args);
+    
+    if (_args.count("help")) {
+      std::cout << desc << "\n";
+      exit(0);    
+    }
+  }
+  catch (std::exception &e){
+    std::cout << e.what() << "\n" << desc << std::endl;
+    exit(1);
   }
   return 0;
 }
