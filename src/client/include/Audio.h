057068c8dfc1221a904ea7579af5c23afacbdd21
nespo_p <nespo_p@833c2cf1-1b31-df11-b905-005056bd6404>
2010-05-09 12:22:23 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@146 833c2cf1-1b31-df11-b905-005056bd6404

--- src/client/include/Audio.h
+++ src/client/include/Audio.h
@@ -8,20 +8,35 @@
 #include <stdio.h>
 #include <map>
 
-#if defined( __WINDOWS_ASIO__ ) || defined( __WINDOWS_DS__ )
-#include <windows.h>
-#define SLEEP( milliseconds ) Sleep( (DWORD) milliseconds )
-#else // Unix variant
-#include <unistd.h>
-#define SLEEP( milliseconds ) usleep( (unsigned long) (milliseconds * 1000.0) )
-#endif
+/* #if defined( __WINDOWS_ASIO__ ) || defined( __WINDOWS_DS__ ) */
+/* #include <windows.h> */
+/* #define SLEEP( milliseconds ) Sleep( (DWORD) milliseconds ) */
+/* #else // Unix variant */
+/* #include <unistd.h> */
+/* #define SLEEP( milliseconds ) usleep( (unsigned long) (milliseconds * 1000.0) ) */
+/* #endif */
+
+typedef signed short  MY_TYPE;
+struct InputData {
+    MY_TYPE* buffer;
+    unsigned long bufferBytes;
+    unsigned long totalFrames;
+    unsigned long frameCounter;
+    unsigned int channels;
+};
 
 class Audio {
  public:
   Audio();
   ~Audio();
+  void					Init();
+  int					getDevices();
+  void					Clean();
  private:
-  RtAudio _audio;
+  RtAudio				_audio;
+  std::vector<RtAudio::DeviceInfo>	_devices;
+  InputData				_inputData;
+  RtAudio::StreamParameters		_inputParams;
 };
 
 #endif // !__AUDIO_H__
