8689abaaebdeb7dda9fa6c89a2895f971a9af25c
petit_y <petit_y@833c2cf1-1b31-df11-b905-005056bd6404>
2010-07-14 17:14:51 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@278 833c2cf1-1b31-df11-b905-005056bd6404

--- src/server/Component_ChannelManager.cpp
+++ src/server/Component_ChannelManager.cpp
@@ -129,7 +129,7 @@ void	Component_ChannelManager::Recv_Join(Packet_v1 const *packet_v1, Session *se
     Send_Join_NOK_ALREADYINCHAN(session, channelId, channelName);
 }
 
-void	Component_ChannelManager::Send_Join_OK(Session *session, field_t channelId, char const * channelName)
+void	Component_ChannelManager::Send_Join_OK(Session *session, field_t channelId, char const *channelName)
 {
   Packet_v1_Channel *packet_v1_channel = new Packet_v1_Channel(CHANNEL_JOIN_OK);
 
@@ -150,7 +150,7 @@ void	Component_ChannelManager::Send_Join_NOK_ALREADYINCHAN(Session *session, fie
 void	Component_ChannelManager::Send_Joined(Session *session, field_t channelId, field_t clientSessionId, char const * clientLogin)
 {
   Packet_v1_Channel *packet_v1_channel = new Packet_v1_Channel(CHANNEL_JOINED);
-  
+
   packet_v1_channel->setChannelId(channelId);
   packet_v1_channel->setClientSessionId(clientSessionId);
   packet_v1_channel->setChannelName(_channelMap->find(channelId)->second->getName());
@@ -164,9 +164,9 @@ void	Component_ChannelManager::Recv_Message(Packet_v1 const *packet_v1, Session 
   Packet_v1_Channel const *packet_v1_channel = 
     static_cast<Packet_v1_Channel const *>(packet_v1);
 
-  field_t channelId = packet_v1_channel->getChannelId();
-  field_t sessionId = packet_v1_channel->getSessionId();
-  char const *message = packet_v1_channel->getMessage();
+  field_t channelId	= packet_v1_channel->getChannelId();
+  field_t sessionId	= packet_v1_channel->getSessionId();
+  char const *message	= packet_v1_channel->getMessage();
 
   if (_channelMap->find(channelId) != _channelMap->end())
     {
@@ -175,10 +175,7 @@ void	Component_ChannelManager::Recv_Message(Packet_v1 const *packet_v1, Session 
 
       std::map<field_t, Session *>::iterator it, end = connected->end();
       for (it = connected->begin(); it != end ; ++it)
-	{
-	  if (it->first != sessionId)
-	    Send_Message_RECV(it->second, message, channelId, sessionId);
-	}
+	Send_Message_RECV(it->second, message, channelId, sessionId);
       Send_Message_ACK(session);
     }
 }
@@ -196,7 +193,6 @@ void	Component_ChannelManager::Send_Message_RECV(Session *session, char const *m
   packet_v1_channel->setMessage(message);
   packet_v1_channel->setClientSessionId(clientSessionId);
 
-
   _serverManager->Send(packet_v1_channel, session);
 }
 
@@ -209,16 +205,16 @@ void	Component_ChannelManager::Recv_Leave(Packet_v1 const *packet_v1, Session *s
   field_t sessionId = session->getSessionId();
   char const *clientLogin = session->getLogin().c_str();
   
-
   if (_channelMap->find(channelId) != _channelMap->end())
     {
       Channel *chan = _channelMap->find(channelId)->second;
       if (chan->removeConnected(sessionId))
 	{
 	  Send_Leave_OK(session, channelId);
+
 	  std::map<field_t, Session*> *connected = chan->getConnected();
-  
-	  std::map<field_t, Session *>::iterator it, end = connected->end();
+ 	  std::map<field_t, Session *>::iterator it, end = connected->end();
+
 	  for (it = connected->begin(); it != end ; ++it)
 	    Send_Leaved(it->second, channelId, sessionId, clientLogin);
 	  if (connected->size() == 0)
@@ -237,7 +233,6 @@ void	Component_ChannelManager::Send_Leave_OK(Session *session, field_t channelId
 
   packet_v1_channel->setChannelId(channelId);
   packet_v1_channel->setChannelName(_channelMap->find(channelId)->second->getName());
-
 
   _serverManager->Send(packet_v1_channel, session);
 }
@@ -259,16 +254,17 @@ void	Component_ChannelManager::Send_Leaved(Session *session, field_t channelId, 
   packet_v1_channel->setChannelName(_channelMap->find(channelId)->second->getName());
   packet_v1_channel->setClientLogin(clientLogin);
 
-
   _serverManager->Send(packet_v1_channel, session);
 }
 
 void	Component_ChannelManager::Disconnect(Session *session)
 {
   m_channel::iterator it, end = _channelMap->end();
+
   for (it = _channelMap->begin(); it != end; ++it)
     {
       std::map<field_t, Session*> *connected = it->second->getConnected();
+
       if (connected->find(session->getSessionId()) != connected->end())
 	{
 	  it->second->removeConnected(session->getSessionId());
@@ -277,6 +273,7 @@ void	Component_ChannelManager::Disconnect(Session *session)
 	  else
 	    {
 	      std::map<field_t, Session*>::iterator i, e = connected->end();
+
 	      for (i = connected->begin(); i != e; ++i)
 		Send_Leaved(i->second, it->first, session->getSessionId(), session->getLogin().c_str());
 	    }
