59f2bcd73a0488e40f1d7ca3570d99b11b60d4d1
nespo_p <nespo_p@833c2cf1-1b31-df11-b905-005056bd6404>
2010-07-17 18:11:51 +0000

    git-svn-id: https://labeip.epitech.eu/svn/2011/livejamming/trunk@343 833c2cf1-1b31-df11-b905-005056bd6404

--- src/client/gui/livejamming/app/configuration_dialog.cpp
+++ src/client/gui/livejamming/app/configuration_dialog.cpp
@@ -34,27 +34,10 @@ ConfigurationDialog::ConfigurationDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ConfigurationDialog)
 {
-    ui->setupUi(this);
-    /*Setting ui for input*/
-    ui->iComboBox->addItems(audio->getIDevices());
-    foreach (int channel, audio->iDevice().supportedChannels())
-        ui->iChannelsComboBox->addItem(QString::number(channel));
-    ui->iCodecsComboBox->addItems(audio->iDevice().supportedCodecs());
-    foreach (int frequency, audio->iDevice().supportedFrequencies())
-        ui->iFrequenciesComboBox->addItem(QString::number(frequency));
-    foreach (int sampleSize, audio->iDevice().supportedSampleSizes())
-        ui->iSampleSizesComboBox->addItem(QString::number(sampleSize));
-    /*Setting ui for output*/
-    ui->outputComboBox->addItems(audio->getODevices());
-    foreach (int channel, audio->oDevice().supportedChannels())
-        ui->oChannelsComboBox->addItem(QString::number(channel));
-    ui->oCodecsComboBox->addItems(audio->oDevice().supportedCodecs());
-    foreach (int frequency, audio->oDevice().supportedFrequencies())
-        ui->oFrequenciesComboBox->addItem(QString::number(frequency));
-    foreach (int sampleSize, audio->oDevice().supportedSampleSizes())
-        ui->oSampleSizesComboBox->addItem(QString::number(sampleSize));
+
 }
 #endif
+
 ConfigurationDialog::~ConfigurationDialog(){
     /*Verify that Engine destructor delete all Audio instances*/
     #ifndef NO_MULTIMEDIA
